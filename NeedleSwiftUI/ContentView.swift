178d05af1fcdbb77b6df91fc8b360cf87c1db182
Karagiorgos, Michalis, Vodafone Greece <michalis.karagiorgos@vodafone.com>
2021-07-22 13:39:24 +0300

    initial commit

---
+++ NeedleSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeedleSwiftUI
+//
+//  Created by Karagiorgos, Michalis, Vodafone Greece on 22/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NeedleSwiftUI/NeedleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeedleSwiftUIApp.swift
+//  NeedleSwiftUI
+//
+//  Created by Karagiorgos, Michalis, Vodafone Greece on 22/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct NeedleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
